dd5d20c598299f8a683eefc19313472011f69775
JlordS32 <jlordsty128@gmail.com>
2023-09-12 18:56:26 +1000

    more updates

--- url-shortening-api-master/script.js
+++ url-shortening-api-master/script.js
@@ -7,29 +7,37 @@ const fetchShortUrl = async (linkToShortened) => {
 	return data;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 	const shortenerBtn = document.getElementById('shortenerBtn');
 	const input = document.querySelector('.link-shorterer-input');
 	const label = document.querySelector('.call-to-action div > label');
 
 	shortenerBtn.addEventListener('click', (e) => {
-      e.preventDefault();
+		e.preventDefault();
 		// Get the input value
 		const inputValue = input.value;
 		console.log(inputValue);
 
+      const isLinkInvalid = isInvalidUrl(inputValue);
+      fetchShortUrl(inputValue).then((link) => {
+         console.log(link);
+      });
+
 		// Validate if it's a valid URL
-		if (isInvalidUrl(inputValue)) {
+		if (isLinkInvalid) {
 			// You can perform your link shortening logic here
 			input.style.border = '2px solid red';
-         label.style.display = 'flex';
+			label.style.display = 'flex';
 		} else {
-         input.style.border = '0';
-         label.style.display = 'none';
+			input.style.border = '0';
+			label.style.display = 'none';
 		}
 	});
 
 	function isInvalidUrl(url) {
+		if (url === '') return true;
+      console.log(url);
+
 		// Regular expression to check if the input is a valid URL
 		var urlPattern = /^(https?:\/\/)?([\w.-]+)(\/[\w.-]*)*\/?$/;
 		return urlPattern.test(url);
