5386b674227b9cf6cc52d8a79d19b9d6a219f826
JlordS32 <jlordsty128@gmail.com>
2023-09-12 19:57:23 +1000

    documented

--- url-shortening-api-master/script.js
+++ url-shortening-api-master/script.js
@@ -1,3 +1,4 @@
+// Function to fetch a shortened URL using the shrtco.de API
 const fetchShortUrl = async (linkToShortened) => {
 	const response = await fetch(
 		`https://api.shrtco.de/v2/shorten?url=${linkToShortened}`
@@ -7,68 +8,77 @@ const fetchShortUrl = async (linkToShortened) => {
 	return data;
 };
 
+// When the DOM (Document Object Model) is fully loaded, execute the following code
 document.addEventListener('DOMContentLoaded', async () => {
-	const shortenerBtn = document.getElementById('shortenerBtn');
-	const input = document.querySelector('.link-shorterer-input');
-	const label = document.querySelector('.call-to-action div > label');
-	const urlContainer = document.querySelector('.url-container');
+	// Get references to various DOM elements using their IDs or classes
+	const shortenerBtn = document.getElementById('shortenerBtn'); // Shorten button
+	const input = document.querySelector('.link-shorterer-input'); // Input field
+	const label = document.querySelector('.call-to-action div > label'); // Validation label
+	const urlContainer = document.querySelector('.url-container'); // Container for displaying shortened URLs
+
+	// Add a click event listener to the "Shorten" button
 	shortenerBtn.addEventListener('click', (e) => {
 		e.preventDefault();
-		// Get the input value
+		// Get the input value and trim any leading/trailing spaces
 		const inputValue = input.value.trim();
 
-		// Validate if link is valid
+		// Validate if the link is invalid
 		const isLinkInvalid = isInvalidUrl(inputValue);
 
-		// Validate if it's a valid URL
+		// Check if it's a valid URL
 		if (isLinkInvalid) {
-			// You can perform your link shortening logic here
-			input.style.border = '2px solid red';
-			label.style.display = 'flex';
+			// Invalid link handling logic
+			input.style.border = '2px solid red'; // Highlight input field in red
+			label.style.display = 'flex'; // Display the validation label
 		} else {
-			input.style.border = '0';
-			label.style.display = 'none';
+			// Valid link handling logic
+			input.style.border = '0'; // Remove the red border
+			label.style.display = 'none'; // Hide the validation label
+			// Fetch and display the shortened URL
 			fetchShortUrl(inputValue).then((link) => {
 				createNewShortLinkContainer(inputValue, link.result.short_link);
 			});
 		}
 	});
 
+	// Function to create a new container for displaying a shortened URL
 	function createNewShortLinkContainer(url, shorturl) {
 		const newShortUrl = document.createElement('div');
 		const originUrl = document.createElement('p');
 		const shortenedUrl = document.createElement('span');
 		const copyBtn = document.createElement('button');
 
-		// Adding classes
+		// Add CSS classes to the elements
 		newShortUrl.classList.add('shortened-link');
 		shortenedUrl.classList.add('textToCopy');
 		copyBtn.classList.add('copyUrl');
 
+		// Set text content for elements
 		shortenedUrl.textContent = shorturl;
 		originUrl.textContent = url;
 		copyBtn.textContent = 'Copy';
 
-		// Appending the new element to the short-url container
+		// Append elements to the new container
 		newShortUrl.appendChild(originUrl);
 		newShortUrl.appendChild(shortenedUrl);
 		newShortUrl.appendChild(copyBtn);
 
-		// Finally putting it into the parent container
+		// Append the new container to the parent container
 		urlContainer.appendChild(newShortUrl);
 	}
 
+	// Function to copy a shortened URL to the clipboard
 	function copyShortUrl(event) {
 		const textToCopy = event.target.previousElementSibling.innerText;
 
 		navigator.clipboard
 			.writeText(textToCopy)
 			.then(() => {
-				event.target.innerText = 'Copied!';
-				event.target.style.backgroundColor = 'hsl(259, 27%, 21%)';
+				event.target.innerText = 'Copied!'; // Change button text to "Copied!"
+				event.target.style.backgroundColor = 'hsl(259, 27%, 21%)'; // Change button color
 				setTimeout(() => {
-					event.target.innerText = 'Copy';
-					event.target.style.backgroundColor = 'hsl(180, 66%, 49%)';
+					event.target.innerText = 'Copy'; // Reset button text to "Copy" after 3 seconds
+					event.target.style.backgroundColor = 'hsl(180, 66%, 49%)'; // Reset button color
 				}, 3000);
 			})
 			.catch((error) => {
@@ -76,20 +86,22 @@ document.addEventListener('DOMContentLoaded', async () => {
 			});
 	}
 
-	// Attach the event listener to the document and use event delegation
+	// Attach a click event listener to the document for event delegation
 	document.addEventListener('click', (event) => {
 		if (event.target.classList.contains('copyUrl')) {
 			copyShortUrl(event);
 		}
 	});
 
+	// Function to validate a URL using a regular expression
 	function isInvalidUrl(url) {
 		if (url === '') return true;
 
 		// Regular expression to check if the input is a valid URL
-		var urlPattern = /^(https?:\/\/)?([\w.-]+\.[A-Za-z]{2,})(\/[\w.-]*)*(\?[\w.-]*=[\w.-]*(&[\w.-]*=[\w.-]*)*)*\/?$/i;
+		var urlPattern =
+			/^(https?:\/\/)?([\w.-]+\.[A-Za-z]{2,})(\/[\w.-]*)*(\?[\w.-]*=[\w.-]*(&[\w.-]*=[\w.-]*)*)*\/?$/i;
 
-      console.log(urlPattern.test(url));
+		// Return true if the URL does not match the pattern
 		return !urlPattern.test(url);
 	}
 });
