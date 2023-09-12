cd1c6168e7d4097b4158cf4a2b976e652a3dc140
JlordS32 <jlordsty128@gmail.com>
2023-09-12 19:49:26 +1000

    some animations and create new shortened link added

--- url-shortening-api-master/script.js
+++ url-shortening-api-master/script.js
@@ -11,17 +11,16 @@ document.addEventListener('DOMContentLoaded', async () => {
 	const shortenerBtn = document.getElementById('shortenerBtn');
 	const input = document.querySelector('.link-shorterer-input');
 	const label = document.querySelector('.call-to-action div > label');
-
+	const copyShortUrlBtn = document.querySelectorAll('.copyUrl');
+	const shortUrlToCopy = document.querySelector('.textToCopy');
+	const urlContainer = document.querySelector('.url-container');
 	shortenerBtn.addEventListener('click', (e) => {
 		e.preventDefault();
 		// Get the input value
 		const inputValue = input.value;
-		console.log(inputValue);
 
-      const isLinkInvalid = isInvalidUrl(inputValue);
-      fetchShortUrl(inputValue).then((link) => {
-         console.log(link);
-      });
+		// Validate if link is valid
+		const isLinkInvalid = isInvalidUrl(inputValue);
 
 		// Validate if it's a valid URL
 		if (isLinkInvalid) {
@@ -31,15 +30,66 @@ document.addEventListener('DOMContentLoaded', async () => {
 		} else {
 			input.style.border = '0';
 			label.style.display = 'none';
+			fetchShortUrl(inputValue).then((link) => {
+				createNewShortLinkContainer(inputValue, link.result.short_link);
+			});
+		}
+	});
+
+	function createNewShortLinkContainer(url, shorturl) {
+		const newShortUrl = document.createElement('div');
+		const originUrl = document.createElement('p');
+		const shortenedUrl = document.createElement('span');
+		const copyBtn = document.createElement('button');
+
+		// Adding classes
+		newShortUrl.classList.add('shortened-link');
+		shortenedUrl.classList.add('textToCopy');
+		copyBtn.classList.add('copyUrl');
+
+		shortenedUrl.textContent = shorturl;
+		originUrl.textContent = url;
+		copyBtn.textContent = 'Copy';
+
+		// Appending the new element to the short-url container
+		newShortUrl.appendChild(originUrl);
+		newShortUrl.appendChild(shortenedUrl);
+		newShortUrl.appendChild(copyBtn);
+
+		// Finally putting it into the parent container
+		urlContainer.appendChild(newShortUrl);
+	}
+
+	function copyShortUrl(event) {
+		const textToCopy = event.target.previousElementSibling.innerText;
+
+		navigator.clipboard
+			.writeText(textToCopy)
+			.then(() => {
+				event.target.innerText = 'Copied!';
+				event.target.style.backgroundColor = 'hsl(259, 27%, 21%)';
+				setTimeout(() => {
+					event.target.innerText = 'Copy';
+					event.target.style.backgroundColor = 'hsl(180, 66%, 49%)';
+				}, 3000);
+			})
+			.catch((error) => {
+				console.error('Unable to copy text: ', error);
+			});
+	}
+
+	// Attach the event listener to the document and use event delegation
+	document.addEventListener('click', (event) => {
+		if (event.target.classList.contains('copyUrl')) {
+			copyShortUrl(event);
 		}
 	});
 
 	function isInvalidUrl(url) {
 		if (url === '') return true;
-      console.log(url);
 
 		// Regular expression to check if the input is a valid URL
-		var urlPattern = /^(https?:\/\/)?([\w.-]+)(\/[\w.-]*)*\/?$/;
-		return urlPattern.test(url);
+		var urlPattern = /^(https?:\/\/)?([\w.-]+\.[a-z]{2,})(\/[\w.-]*)*\/?$/i;
+		return !urlPattern.test(url);
 	}
 });
