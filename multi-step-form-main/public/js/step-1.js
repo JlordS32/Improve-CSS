46410c70218fef36f0a4e2cbc8d8deec4956dda0
JlordS32 <jlordsty128@gmail.com>
2023-09-19 22:30:25 +1000

    writing the js, doing validation first

--- multi-step-form-main/public/js/step-1.js
+++ multi-step-form-main/public/js/step-1.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
+	// Regex for validation
+	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	const nameRegex = /^[A-Za-z\s]+$/;
+	const phoneRegex = /^(?:\+\d{1,3})?\d{10}$/;
+
 	const formBtnElement = document.querySelector('.form button');
+	const nameInputElement = document.querySelector('.form input[name="name"]');
+	const phoneInputElement = document.querySelector('.form input[name="phone"]');
+	const emailInputElement = document.querySelector(
+		'.form input[name="email-address"]'
+	);
 
 	formBtnElement.addEventListener('click', () => {
-		window.location.href = './public/step/2.html';
+		console.log('Email', emailRegex.test(emailInputElement.value));
+
+		if (emailRegex.test(emailInputElement.value)) {
+			window.location.href = './public/step/2.html';
+		}
 	});
 });
--- multi-step-form-main/public/js/step-3.js
+++ multi-step-form-main/public/js/step-3.js
@@ -5,6 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
 	const backBtnElement = document.querySelector(
 		'.selection-container .back-btn'
 	);
+	const extraElement = document.querySelectorAll('.extra');
+
+	extraElement.forEach((extra) => {
+		extra.addEventListener('click', () => {
+			const checkboxElement = extra.querySelector('.checkbox');
+
+			if (checkboxElement.classList.contains('checked')) {
+				checkboxElement.classList.remove('checked');
+				checkboxElement.querySelector('img').style.display = 'none';
+            extra.style.outline = '2px solid hsl(231, 11%, 63%)';
+            extra.style.backgroundColor = 'hsl(0, 0%, 100%)';
+            
+			} else {
+				checkboxElement.classList.add('checked');
+				checkboxElement.querySelector('img').style.display = 'flex';
+            extra.style.outline = '2px solid hsl(213, 96%, 18%)';
+            extra.style.backgroundColor = 'hsl(230, 75%, 98%)';
+			}
+		});
+	});
 
 	nextBtnElement.addEventListener('click', () => {
 		window.location.href = './4.html';
