fec9fcca45ed696ca2d5c132b04d08d002d8114c
JlordS32 <jlordsty128@gmail.com>
2023-09-21 22:18:13 +1000

    in progress

--- multi-step-form-main/public/js/step-4.js
+++ multi-step-form-main/public/js/step-4.js
@@ -2,13 +2,32 @@ function capitalizeFirstLetter(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function createAddOnElement(addOns, addOnsElement) {
+function createAddOnElement(addOns, addOnsElement, userPlan) {
 	addOns.forEach((addOn) => {
 		const addOnElement = document.createElement('div');
-		addOnElement.innerHTML = `<p class="add-on-name">${addOn.name}</p> <p class="add-on-price">${addOn.price}</p>`;
+		const price = parseFloat(addOn.price.toString().match(/\d+(\.\d+)?/)[0]);
+
+		addOnElement.innerHTML = `<p class="add-on-name">${
+			addOn.name
+		}</p> <p class="add-on-price">$${
+			userPlan.frequency === 'yearly' ? price * 10 : price
+		}/${userPlan.frequency === 'yearly' ? 'yr' : 'mo'}</p>`;
 
 		addOnsElement.appendChild(addOnElement);
 	});
+}
+
+function calculateTotal(addOns, userPlan) {
+	let total = 0;
+
+	addOns.forEach((addOn) => {
+		const price = parseFloat(addOn.price.toString().match(/\d+(\.\d+)?/)[0]);
+		total += price;
+	});
+
+	total += userPlan.price;
+
+	console.log(total);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,7 +45,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		userPlan.frequency === 'yearly' ? 'yr' : 'mo'
 	}`;
 
-	createAddOnElement(addOns, extraAddOnsElement);
+	createAddOnElement(addOns, extraAddOnsElement, userPlan);
+
+	// Callling function to calculate total
+	calculateTotal(addOns, userPlan);
 
 	const backBtnElement = document.querySelector(
 		'.selection-container .back-btn'
