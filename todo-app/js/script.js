9ab1f8f61129cae1f6a9c015577007c7e981f341
JlordS32 <jlordsty128@gmail.com>
2023-10-01 21:06:33 +1100

    used formkit/auto-animate

--- todo-app/js/script.js
+++ todo-app/js/script.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import autoAnimate from 'https://cdn.jsdelivr.net/npm/@formkit/auto-animate@1.0.0-beta.1/index.min.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const todoCheckBoxElement = document.querySelectorAll('.todo div');
@@ -6,20 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	const todosElement = document.querySelector('.todos');
 	const newTodoElement = document.querySelector('.newtodo input[type="text"]');
 
+   autoAnimate(document.querySelector('.todos'));
+
 	todoCheckBoxElement.forEach((checkbox) => {
 		checkbox.addEventListener('click', () => {
-			if (checkbox.classList.contains('checked')) {
-				checkbox.classList.remove('checked');
-				checkbox.innerHTML = '';
-				return;
-			}
-
-			checkbox.classList.add('checked');
-
-			// Creating checked image element
-			const imgElement = document.createElement('img');
-			imgElement.setAttribute('src', './images/icon-check.svg');
-			checkbox.appendChild(imgElement);
+			toggleCheckbox(checkbox);
 		});
 	});
 
@@ -27,9 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		if (e.key === 'Enter' && newTodoElement.value !== '') {
 			const newTodo = document.createElement('div');
 			newTodo.classList.add('todo');
-			newTodo.innerHTML = `<div class="checkbox" data-id="${uuidv4()}"></div><p>${
-				newTodoElement.value
-			}</p><img src="./images/icon-cross.svg" alt="close-todo"/>`;
+			newTodo.setAttribute('data-id', uuidv4());
+			newTodo.innerHTML = `<div class="checkbox"></div><p>${newTodoElement.value}</p><img src="./images/icon-cross.svg" alt="close-todo"/>`;
 
 			todosElement.appendChild(newTodo);
 
@@ -41,7 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
 				toggleCheckbox(newTodo.querySelector('.checkbox'));
 			});
 
+			newTodo.querySelector('img').addEventListener('click', () => {
+				newTodo.remove();
+			});
+
 			todoStateElement.style.display = 'flex';
+			todoStateElement.style.boxShadow = '0px 40px 40px 24px rgba(0,0,0,0.1)';
 		}
 	});
 
@@ -58,10 +54,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		// Creating checked image element
 		const imgElement = document.createElement('img');
 		imgElement.setAttribute('src', './images/icon-check.svg');
+		imgElement.classList.add('animate--fast');
+		imgElement.classList.add('fadeIn');
 		checkbox.appendChild(imgElement);
-	}
-
-	function removeElement(element) {
-		element.remove();
 	}
 });
