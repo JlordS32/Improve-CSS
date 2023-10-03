50b57051e6b164461cf5917ae556fb2c9bb8e795
JlordS32 <jlordsty128@gmail.com>
2023-10-03 12:11:52 +1100

    making some progres

--- interactive-comments/js/script.js
+++ interactive-comments/js/script.js
@@ -13,18 +13,31 @@ async function loadJSON() {
 	}
 }
 
+function scrollToBottom() {
+	const commentsContainer = document.querySelector('.comments-container');
+	// Scroll all the way to the bottom
+	commentsContainer.scrollTop = commentsContainer.scrollHeight;
+}
+
 function createCommentElement(
 	score,
 	image,
 	username,
 	createdDate,
-	commentContent
+	commentContent,
+	isUser = false
 ) {
+   const commentWrapper = document.createElement('div');
 	const commentElement = document.createElement('div');
 	const commentsContainer = document.querySelector('.comments-container');
 
+   commentWrapper.classList.add('comment-wrapper');
 	commentElement.classList.add('comment');
 	commentElement.setAttribute('data-id', uuidv4());
+
+	if (isUser) {
+		commentElement.classList.add('my-comment');
+	}
 
 	commentElement.innerHTML = `
          <div class="vote-comment">
@@ -38,31 +51,91 @@ function createCommentElement(
                </div>
             </div>
          </div>
-         <div class="wrapper"> 
-            <div class="commenter"> 
-               <div class="user">
-                  <div class="user-img">
-                     <img src="${image}" alt="user-img" />
-                  </div>
-                  <div class="user-name">
-                     ${username}
-                  </div>
-                  <div class="comment-created">
-                     <p>${createdDate}</p>
-                  </div>
-               </div>
-               <div class="reply">
-                  <img src="./images/icon-reply.svg" alt="reply" />
-                  <p>Reply</p>
-               </div>
+         <div class="user">
+            <div class="user-img">
+               <img src="${image}" alt="user-img" />
             </div>
-            <div class="comment-content">
-               ${commentContent}
+            <div class="user-name">
+               ${username}
             </div>
+            <div class="comment-created">
+               <p>${createdDate}</p>
+            </div>
+         </div>
+         <div class="reply">
+            <img src="./images/icon-reply.svg" alt="reply" />
+            <p>Reply</p>
+         </div>
+         <div class="comment-content">
+            ${commentContent}
          </div>
       `;
 
-	commentsContainer.appendChild(commentElement);
+	if (commentElement.classList.contains('my-comment')) {
+		const userElement = commentElement.querySelector('.user');
+
+		const youElement = document.createElement('div');
+		youElement.classList.add('you-tag');
+		youElement.textContent = 'you';
+
+		userElement.insertBefore(
+			youElement,
+			userElement.querySelector('.comment-created')
+		);
+
+		// Delete Button
+		const deleteBtn = document.createElement('div');
+		deleteBtn.classList.add('delete-comment');
+		deleteBtn.innerHTML = `
+         <img src="./images/icon-delete.svg" alt="delete" />
+         <p>Delete</p>
+      `;
+		deleteBtn.addEventListener('click', () => {
+			commentWrapper.remove();
+		});
+
+		// Edit Button
+		const editBtn = document.createElement('div');
+		editBtn.classList.add('edit-comment');
+		editBtn.innerHTML = `
+         <img src="./images/icon-edit.svg" alt="edit" />
+         <p>Edit</p>
+      `;
+		editBtn.addEventListener('click', () => {
+			const updateComment = document.createElement('textarea');
+			const updateBtn = document.createElement('div');
+			const commentContentElement =
+				commentElement.querySelector('.comment-content');
+
+			// Updating styling
+			commentContentElement.style.display = 'none';
+			updateBtn.classList = 'update-comment-btn';
+			updateBtn.textContent = 'update';
+
+			commentElement.appendChild(updateComment);
+			commentElement.appendChild(updateBtn);
+
+			scrollToBottom();
+
+			updateBtn.addEventListener('click', () => {
+            if (updateComment.value.trim() === '') return;
+
+				// Update user comment value
+				commentContentElement.textContent = updateComment.value;
+
+				// Update styling
+				updateComment.style.display = 'none';
+				updateBtn.style.display = 'none';
+				commentContentElement.style = 'block';
+			});
+		});
+
+		commentElement.appendChild(deleteBtn);
+		commentElement.appendChild(editBtn);
+	}
+
+   commentWrapper.appendChild(commentElement);
+	commentsContainer.appendChild(commentWrapper);
 }
 
 function sanitizeHTML(str) {
@@ -72,11 +145,14 @@ function sanitizeHTML(str) {
 }
 document.addEventListener('DOMContentLoaded', async () => {
 	const commentsContainer = document.querySelector('.comments-container');
-   autoAnimate(commentsContainer);
+	autoAnimate(commentsContainer);
+
 	const newCommentBtn = document.querySelector('.my-comment-container button');
 	const newCommentInput = document.querySelector(
 		'.my-comment-container textarea'
 	);
+
+   const voteWrapperElement = document.querySelectorAll('.vote-wrapper');
 
 	const data = await loadJSON();
 	const { currentUser, comments } = data;
@@ -94,19 +170,30 @@ document.addEventListener('DOMContentLoaded', async () => {
 	});
 
 	newCommentBtn.addEventListener('click', () => {
+		if (newCommentInput.value.trim() == '') return;
+
 		createCommentElement(
 			0,
 			currentUser.image.png,
 			currentUser.username,
 			'2 days ago',
-			sanitizeHTML(newCommentInput.value)
+			sanitizeHTML(newCommentInput.value),
+			true
 		);
 
+		scrollToBottom();
 
-      // Scroll all the way to the bottom
-      commentsContainer.scrollTop = commentsContainer.scrollHeight;
-
-      // Clear input
-      newCommentInput.value = '';
+		// Clear input
+		newCommentInput.value = '';
 	});
+
+   voteWrapperElement.forEach(voteElement => {
+      const upvote = voteElement.querySelector('.upvote');
+      const downvote = voteElement.querySelector('.downvote');
+      const voteCount = voteElement.querySelector('.vote-number');
+
+      upvote.addEventListener('click', () => {
+         console.log(voteCount.textContent);
+      })
+   })
 });
