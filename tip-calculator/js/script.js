7ca9906c2e6ba6881cb7ba43b84f37b6030a2bd9
JlordS32 <jlordsty128@gmail.com>
2023-10-07 17:40:44 +1100

    final commit for this project

--- tip-calculator/js/script.js
+++ tip-calculator/js/script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	percentageElement
 		.querySelector('.custom-percentage input')
-		.addEventListener('input', (e) => {
+		.addEventListener('change', (e) => {
 			const isGreaterThanNegative = parseInt(e.target.value) > -1;
 
 			if (isGreaterThanNegative) {
